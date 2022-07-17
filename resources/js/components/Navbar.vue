e62081b6393c2ffd8812dff65cf7fc6d7e3434d2
MatheusFontenele <matheus.fontenele@icloud.com>
2022-07-16 23:43:42 -0300

    feat: create second header

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,9 +1,42 @@
 <template>
     <header class="w-screen h-20 bg-[#03ABF8] ">
         <div class=" h-full px-20 flex items-center justify-between max-w-7xl mx-auto">
-            <ph-list :size="38" weight="bold" color="#FFF"  />
+            <button class="p-2 rounded-lg hover:brightness-125 hover:bg-slate-600" >
+                <ph-list :size="38" weight="bold" color="#FFF"  />
+            </button>
+
             <img src="../../images/logo.png" alt="">
-            <ph-magnifying-glass :size="38" color="#f7f7f7" weight="bold"/>
+
+            <button class="p-2 rounded-lg hover:brightness-125 hover:bg-slate-600" >
+                <ph-magnifying-glass :size="38" color="#f7f7f7" weight="bold"/>
+            </button>
+
+        </div>
+        <div
+            style="background: linear-gradient(90deg,#f36300 50%,#622181 0)" class="w-full h-4"
+        >
+            <div class="w-full px-20 max-w-7xl mx-auto">
+                <ul class="flex mx-auto justify-between ">
+                    <li class="bg-[#f36300] w-full h-4 hover:h-8 transition-[height]">
+                        <a class="opacity-0 h-full flex items-center justify-center hover:opacity-100 text-xl text-slate-200 font-medium" href="">Na mira</a>
+                    </li>
+                    <li class="bg-[#126D97] w-full h-4  hover:h-8 transition-[height]">
+                        <a class="opacity-0 h-full flex items-center justify-center hover:opacity-100 text-xl text-slate-200 font-medium" href="">Noticias</a>
+                    </li>
+                    <li class="bg-[#B30000] w-full h-4 hover:h-8 transition-[height]">
+                        <a class="opacity-0 h-full flex items-center justify-center hover:opacity-100 text-xl text-slate-200 font-medium" href="">Culinaria</a>
+                    </li>
+                    <li class="bg-[#383D9E] w-full h-4 hover:h-8 transition-[height]">
+                        <a class="opacity-0 h-full flex items-center justify-center hover:opacity-100 text-xl text-slate-200 font-medium" href="">Politica</a>
+                    </li>
+                    <li class="bg-[#728A00] w-full h-4 hover:h-8 transition-[height]">
+                        <a class="opacity-0 h-full flex items-center justify-center hover:opacity-100 text-xl text-slate-200 font-medium" href="">Esports</a>
+                    </li>
+                    <li class="bg-[#622181] w-full h-4 hover:h-8 transition-[height]">
+                        <a class="opacity-0 h-full flex items-center justify-center hover:opacity-100 text-xl text-slate-200 font-medium" href="">Podcast</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </header>
 </template>
